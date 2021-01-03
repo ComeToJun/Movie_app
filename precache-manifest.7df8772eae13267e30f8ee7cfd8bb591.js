770add518a90ab08eba1b2d7f955ad9cbc94b851
JunWorld <65802905+ComeToJun@users.noreply.github.com>
2021-01-03 16:30:10 +0900

    Updates

---
+++ precache-manifest.7df8772eae13267e30f8ee7cfd8bb591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4a5a11e8c70ca4572893c0aefef496",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d6033076eb97066574f3",
+    "url": "/movie_app/static/css/main.f26d0556.chunk.css"
+  },
+  {
+    "revision": "1e9944beececbe245826",
+    "url": "/movie_app/static/js/2.8d1df91e.chunk.js"
+  },
+  {
+    "revision": "d6033076eb97066574f3",
+    "url": "/movie_app/static/js/main.11ba692a.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
